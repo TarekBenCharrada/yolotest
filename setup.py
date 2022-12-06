a0220234afb38962b48a8ad457050f48374a996c
Tarek BEN CHARRADA <t.ben-charrada@reezocar.com>
2022-12-06 16:48:55 +0100

    test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='yolotest',
+    version='',
+    packages=['yolov'],
+    url='',
+    license='',
+    author='wali',
+    author_email='',
+    description=''
+)
