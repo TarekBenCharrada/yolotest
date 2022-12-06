3c3542c8735c7721bfeb1dbd8f1a984a1b816a37
Tarek BEN CHARRADA <t.ben-charrada@reezocar.com>
2022-12-06 17:01:18 +0100

    test

---
+++ yolov7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='yolov7',
+    version='',
+    packages=['utils', 'utils.aws', 'utils.wandb_logging', 'models'],
+    url='',
+    license='',
+    author='wali',
+    author_email='',
+    description=''
+)
