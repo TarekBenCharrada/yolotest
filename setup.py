6ed805b2116486d472c456a0ef3549b000314a5c
TarekBenCharrada <109971949+TarekBenCharrada@users.noreply.github.com>
2022-12-30 10:24:47 +0100

    [FIX] Fix typo in setup.py + add other subfolders.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='yolov7',
     version='',
-    packages=['yolov7','yolov7.models', 'yolov7.mtools', 'yolov7.utils'],
+    packages=['yolov7','yolov7.models', 'yolov7.tools', 'yolov7.utils', 'yolov7.cfg', 'yolov7.data'],
     url='',
     license='',
     author='wali',
