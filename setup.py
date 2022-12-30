7b706d59913285da299e6e1115db418701794561
TarekBenCharrada <109971949+TarekBenCharrada@users.noreply.github.com>
2022-12-30 10:51:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup ,  find_packages
 setup(
     name='yolov7',
     version='',
-    packages= find_packages (['yolov7','yolov7.models', 'yolov7.tools', 'yolov7.utils', 'yolov7.cfg', 'yolov7.data]),
+    packages= find_packages (['yolov7','yolov7.models', 'yolov7.tools', 'yolov7.utils', 'yolov7.cfg', 'yolov7.data']),
     url='',
     license='',
     author='wali',
