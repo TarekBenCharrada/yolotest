7fb7cca20d6d52d3fae761ba4bc3035ac2710fc1
TarekBenCharrada <109971949+TarekBenCharrada@users.noreply.github.com>
2022-12-30 10:23:14 +0100

    [FIX] Add subfolders to setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='yolov7',
     version='',
-    packages=['yolov7','yolov7.mdels'],
+    packages=['yolov7','yolov7.models', 'yolov7.mtools', 'yolov7.utils'],
     url='',
     license='',
     author='wali',
